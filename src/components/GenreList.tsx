1b48a7fcc5aa58cf870977a989bdaedb37a5f444
danel <danel.caballero@aersale.com>
2023-05-24 12:32:31 -0400

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,20 @@
+import { ListItem, List, HStack, Image, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
 
-const GenreList= () => {
+const GenreList = () => {
 
-  const {data} = useGenres();
+  const { data } = useGenres();
 
   return (
-    <ul>
-    {data.map(genre => <li key = {genre.id}>{genre.name}</li>)}
-    </ul>
+    <List>
+      {data.map(genre => <ListItem key={genre.id} padding='5px'><HStack>
+        
+        <Image boxSize = '32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+        <Text fontSize='lg'>{genre.name}</Text>
+
+        </HStack></ListItem>)}
+    </List>
   )
 }
 
