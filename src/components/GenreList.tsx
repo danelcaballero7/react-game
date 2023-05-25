897ca4383c6866ebe32753b67e827b542249aab4
danel <danel.caballero@aersale.com>
2023-05-25 16:35:19 -0400

    using react-query for fetch Platforms

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -20,7 +20,7 @@ const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
     <>
     <Heading fontSize ='2xl' marginBottom={3}>Genres</Heading>
     <List>
-      {data.map(genre => <ListItem key={genre.id} padding='5px'><HStack>
+      {data?.results.map(genre => <ListItem key={genre.id} padding='5px'><HStack>
         
         <Image boxSize = '32px' borderRadius={8} objectFit='cover' src={getCroppedImageUrl(genre.image_background)} />
         <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id? 'bold': 'normal'} onClick={() => onSelectGenre(genre) } fontSize='lg' variant='link'>{genre.name}</Button>
