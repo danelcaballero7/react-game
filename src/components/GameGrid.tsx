3c9d280b411faeb3f46c5f48345c543f4d42d8cd
danel <danel.caballero@aersale.com>
2023-05-25 17:17:21 -0400

    Fetching games with react-query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,13 +10,12 @@ interface Props{
     gameQuery: GameQuery;    
 }
 
-
 const GameGrid = ({gameQuery}: Props) => {
 
     const { data, error, isLoading } = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6];
 
-    if (error) return <Text>{error}</Text>
+    if (error) return <Text>{error.message}</Text>
 
 
     return (
@@ -29,7 +28,7 @@ const GameGrid = ({gameQuery}: Props) => {
                     skeletons.map((skeleton) => (<GameCardContainer> <GameCardSkeleton  />
                     </GameCardContainer>
                     ))}
-                {data.map(game => (
+                {data?.results.map(game => (
                     <GameCardContainer key={game.id}>
                         <GameCard  game={game} />
                     </GameCardContainer>))}
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 
- export default axios.create({
-    baseURL:'https://api.rawg.io/api',
-    params:{
-        key: '//Aqui va la clave---------------------------------------------------------------------'
+export interface FetchResponse<T> {
+    count: number;
+    results: T[];
+}
+
+export default axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: '027fedfba7894183a56eccdab6b6fa85'
 
     }
 })
