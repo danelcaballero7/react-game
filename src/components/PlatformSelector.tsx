0bbde2585bdf21a7da29d443a6f7405700e1bdae
danel <danel.caballero@aersale.com>
2023-05-25 17:25:45 -0400

    deleting dependencies

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import {Button, Menu, MenuButton, MenuItem, MenuList} from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 import usePlatforms from '../hooks/usePlatforms';
