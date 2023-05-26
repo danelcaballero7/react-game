8d0c0d7f0f1ba76828c002d730d046db3e964ee8
danel <danel.caballero@aersale.com>
2023-05-26 14:41:43 -0400

    Refactor - simplify time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres"
 import APIClient from "../services/api-client";
+import ms from 'ms';
 
 
 const apiClient = new APIClient<Genre>('/genres');
@@ -14,7 +14,7 @@ export interface Genre {
 const useGenres = () => useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms('24h'),
     // initialData: {count: genres.length, results: genres, next: null}
 });
 export default useGenres 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 
 import { useQuery } from "@tanstack/react-query";
-import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
+import ms from 'ms';
 
 const apiClient = new APIClient<Platform>('/platforms/list/parents')
 
@@ -13,7 +13,7 @@ export interface Platform {
 const usePlatform = () => useQuery({
     queryKey:['platforms'],
     queryFn:apiClient.getAll,
-    staleTime: 24*60*60*1000,
+    staleTime: ms('24h'),
     // initialData: {count: platforms.length, results: platforms, next:null}
 })
 
