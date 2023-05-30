a36b8a91c446560878dcd88709060203d7594a1b
danel <danel.caballero@aersale.com>
2023-05-30 17:51:45 -0400

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import { Heading,Spinner} from '@chakra-ui/react';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
 import GameTrailer from '../components/GameTrailer';
+import GameScreenshots from '../components/GameScreenshots';
 
 const GameDetailPage = () => {
 
@@ -20,7 +21,8 @@ const GameDetailPage = () => {
       <Heading>{game?.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} /> 
-      <GameTrailer gameId={game.id}/>     
+      <GameTrailer gameId={game.id}/>    
+      <GameScreenshots gameId={game.id} /> 
 
     </>
   )
