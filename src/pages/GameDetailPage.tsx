f21a9acbda2be56c03f798cea89678eea42929f6
danel <danel.caballero@aersale.com>
2023-05-30 17:28:34 -0400

    build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
+
 import { useParams } from 'react-router-dom';
 import useGame from '../hooks/useGame';
-import { Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
+import { Heading,Spinner} from '@chakra-ui/react';
 import ExpandableText from '../components/ExpandableText';
-import DefinitionItem from '../components/DefinitionItem';
-import CriticScore from '../components/CriticScore';
 import GameAttributes from '../components/GameAttributes';
+import GameTrailer from '../components/GameTrailer';
 
 const GameDetailPage = () => {
 
@@ -20,7 +19,9 @@ const GameDetailPage = () => {
     <>
       <Heading>{game?.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />      
+      <GameAttributes game={game} /> 
+      <GameTrailer gameId={game.id}/>     
+
     </>
   )
 }
