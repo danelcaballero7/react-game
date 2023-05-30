1442988e29fd390f788d5783d02ccc6015d6476a
danel <danel.caballero@aersale.com>
2023-05-30 13:06:51 -0400

    fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery} from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client"
 import { Platform } from "./usePlatforms";
 import ms from 'ms'
@@ -10,6 +10,8 @@ const apiClient = new APIClient<Game>('/games');
 export interface Game {
     id: number;
     name: string;
+    slug: string;
+    description_raw: string;
     background_image: string;
     parent_platforms: { platform: Platform }[];
     metacritic: number;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,13 +1,16 @@
+import { Box } from '@chakra-ui/react'
 import Navbar from '../components/NavBar'
 import { Outlet } from 'react-router-dom'
 
 const layout = () => {
   return (
     <>
-    
-    <Navbar />
-    <Outlet />
-    
+
+      <Navbar />
+      <Box padding={5}>
+        <Outlet />
+      </Box>
+
     </>
   )
 }
